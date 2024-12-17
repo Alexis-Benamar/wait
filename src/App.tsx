a1c0ad543066c3ac743f614f6accc71258e93287
Alexis-Benamar <alexis.benamar@viacesi.fr>
2024-12-17 10:25:15 +0100

    refactor out lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
-import React, { lazy } from 'react'
+import React from 'react'
 
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+import Bash from 'pages/Bash'
+import Ffmpeg from 'pages/Ffmpeg'
+import Git from 'pages/Git'
 import Home from 'pages/Home'
 import NotFound from 'pages/NotFound'
 import Root from 'pages/Root'
+import Weight from 'pages/Weight'
 
 import './reset.css'
 import './style.css'
-
-// import Weight from 'pages/Weight'
-
-const Bash = lazy(() => import('pages/Bash'))
-const Ffmpeg = lazy(() => import('pages/Ffmpeg'))
-const Git = lazy(() => import('pages/Git'))
-const Weight = lazy(() => import('pages/Weight'))
 
 const router = createBrowserRouter([
   {
